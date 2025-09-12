75d95e9f4c2253418c93763094e015157a974bd6
rhdholariya <rahuldholariya001@gmail.com>
2025-09-12 12:51:25 +0530

    Add bootstrap vue

--- src/main.js
+++ src/main.js
@@ -8,10 +8,14 @@ import Prism from "prismjs";
 console.log(Prism);
 import "prismjs/themes/prism.css";
 
+// Bootstrap Vue Next
+import { createBootstrap } from 'bootstrap-vue-next'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 import '@/assets/scss/style.scss';
 app.component("Breadcrumbs", Breadcrumbs);
 app.use(router)
 app.use(FlagIcon)
+app.use(createBootstrap())
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import { PhBriefcase, PhCode } from "@phosphor-icons/vue";
 import CodeHighlight from "@components/prism/CodeHighlight.vue";
 import { ref, nextTick } from "vue";
 import { html as beautifyHtml } from "js-beautify";
+import {BAlert, BBadge, BButton, BCard, BCarousel, BCarouselSlide, BCollapse} from "bootstrap-vue-next";
 
 //  Basic alerts data
 const alerts = [
@@ -43,6 +44,19 @@ const alertBlock = ref(null);
 const outlineBlock = ref(null);
 const capturedCode = ref("");
 const capturedOutlineCode = ref("");
+const slide  = ref(0);
+const sliding  = ref(false);
+// Collapse state
+const showCollapse = ref(false);
+const showInnerCollapse = ref(false);
+
+const onSlideStart = (slide)=> {
+  sliding.value = true
+}
+
+const onSlideEnd = (slide) => {
+  sliding.value = false
+}
 
 // Capture basic HTML
 const captureHtml = async () => {
@@ -122,6 +136,78 @@ const captureOutlineHtml = async () => {
               </div>
             </div>
           </div>
+        </div>
+
+        <b-alert show variant="primary">Primary Alert</b-alert>
+        <h2>Example heading <b-badge>New</b-badge></h2>
+        <div>
+          <b-button @click="showCollapse = !showCollapse" variant="primary">Toggle Collapse</b-button>
+          <b-collapse v-model="showCollapse" class="mt-2">
+            <b-card>
+              <p class="card-text">Collapse contents Here</p>
+              <b-button @click="showInnerCollapse = !showInnerCollapse" size="sm">Toggle Inner Collapse</b-button>
+              <b-collapse v-model="showInnerCollapse" class="mt-2">
+                <b-card>Hello!</b-card>
+              </b-collapse>
+            </b-card>
+          </b-collapse>
+        </div>
+        <div>
+          <b-carousel
+              id="carousel-1"
+              v-model="slide"
+              :interval="4000"
+              controls
+              indicators
+              background="#ababab"
+              img-width="1024"
+              img-height="480"
+              style="text-shadow: 1px 1px 2px #333;"
+              @sliding-start="onSlideStart"
+              @sliding-end="onSlideEnd"
+          >
+            <!-- Text slides with image -->
+            <b-carousel-slide
+                caption="First slide"
+                text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                img-src="https://picsum.photos/1024/480/?image=52"
+            ></b-carousel-slide>
+
+            <!-- Slides with custom text -->
+            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+              <h1>Hello world!</h1>
+            </b-carousel-slide>
+
+            <!-- Slides with image only -->
+            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+            <!-- Slides with img slot -->
+            <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+            <b-carousel-slide>
+              <template #img>
+                <img
+                    class="d-block img-fluid w-100"
+                    width="1024"
+                    height="480"
+                    src="https://picsum.photos/1024/480/?image=55"
+                    alt="image slot"
+                >
+              </template>
+            </b-carousel-slide>
+
+            <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+            <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+                a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+              </p>
+            </b-carousel-slide>
+          </b-carousel>
+
+          <p class="mt-4">
+            Slide #: {{ slide }}<br>
+            Sliding: {{ sliding }}
+          </p>
         </div>
 
         <!-- Outline Alert -->
