a97dbc674dcffe7d9255e88addc797432cec3c16
rhdholariya <rahuldholariya001@gmail.com>
2025-09-16 17:25:42 +0530

    worked in code merge

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
+
+<template>
+    <RouterView />
+</template>
+
 <script setup>
-import { RouterView } from 'vue-router';
-
-import HeaderMain from '@/components/layouts/header/index.vue';
-import Sidebar from '@/components/layouts/sidebar/index.vue';
-import FooterSection from '@/components/layouts/footer/index.vue';
-import {ref} from "vue";
-
-const isSemiNav = ref(false);
-
-
-const toggleNav = () => {
-  isSemiNav.value = !isSemiNav.value;
-};
-
-const closeSemiNav = () => {
-  isSemiNav.value = false;
-};
 
 import { onMounted } from "vue";
 
@@ -31,13 +19,5 @@ onMounted(() => {
     head.appendChild(link);
   });
 });
-
-</script>
-
-<template>
-    <RouterView />
-</template>
-
-<script setup>
 import 'bootstrap/dist/css/bootstrap.min.css';
 </script>
--- src/views/AppLayout.vue
+++ src/views/AppLayout.vue
@@ -1,3 +1,17 @@
+
+<script setup>
+import { ref } from "vue";
+import Sidebar from "@/components/layouts/sidebar/index.vue";
+import HeaderMain from "@/components/layouts/header/index.vue";
+import FooterSection from "@/components/layouts/footer/index.vue";
+import Customizer from '@/components/Customizer/index.vue';
+
+const isSemiNav = ref(false);
+const toggleNav = () => (isSemiNav.value = !isSemiNav.value);
+const closeSemiNav = () => (isSemiNav.value = false);
+</script>
+
+
 <template>
     <div class="app-wrapper">
         <Sidebar :class="{ 'semi-nav': isSemiNav }" />
@@ -5,18 +19,8 @@
             <HeaderMain @toggle-nav="toggleNav" @close-nav="closeSemiNav" />
             <slot/>
             <FooterSection />
+          <Customizer />
         </div>
     </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import { RouterView } from "vue-router";
-import Sidebar from "@/components/layouts/sidebar/index.vue";
-import HeaderMain from "@/components/layouts/header/index.vue";
-import FooterSection from "@/components/layouts/footer/index.vue";
-
-const isSemiNav = ref(false);
-const toggleNav = () => (isSemiNav.value = !isSemiNav.value);
-const closeSemiNav = () => (isSemiNav.value = false);
-</script>
